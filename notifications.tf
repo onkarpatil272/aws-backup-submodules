44cf6e91d6d7c88fe0c50c5e57dc3ed5afa1949c
omi <omi@gmail.com>
2025-07-25 15:30:43 +0530

    update file

--- notifications.tf
+++ notifications.tf
@@ -1,7 +1,9 @@
 
 # IAM Policy to allow AWS Backup to publish to SNS topics
 data "aws_iam_policy_document" "sns" {
-  for_each = var.enabled && !var.notifications_disable_sns_policy ? var.notifications : {}
+  for_each = var.enabled && !var.notifications_disable_sns_policy ? {
+    for k, v in var.notifications : k => v if try(v.enabled, false)
+  } : {}
 
   statement {
     actions   = ["SNS:Publish"]
@@ -19,7 +21,9 @@ data "aws_iam_policy_document" "sns" {
 
 # SNS Topic Policies
 resource "aws_sns_topic_policy" "sns" {
-  for_each = var.enabled && !var.notifications_disable_sns_policy ? var.notifications : {}
+  for_each = var.enabled && !var.notifications_disable_sns_policy ? {
+    for k, v in var.notifications : k => v if try(v.enabled, false)
+  } : {}
 
   arn    = each.value.sns_topic_arn
   policy = data.aws_iam_policy_document.sns[each.key].json
@@ -27,7 +31,9 @@ resource "aws_sns_topic_policy" "sns" {
 
 # Backup Vault Notifications
 resource "aws_backup_vault_notifications" "this" {
-  for_each = var.enabled ? var.notifications : {}
+  for_each = var.enabled ? {
+    for k, v in var.notifications : k => v if try(v.enabled, false)
+  } : {}
 
   backup_vault_name   = var.vault_name != null ? var.vault_name : "Default"
   sns_topic_arn       = each.value.sns_topic_arn
@@ -78,21 +84,4 @@ resource "aws_cloudwatch_metric_alarm" "backup_failure_alarm" {
     }
   }
   tags= local.common_tags
-}
-
-
-# Support custom CloudWatch alarms from var.cloudwatch_alarms
-resource "aws_cloudwatch_metric_alarm" "custom" {
-  for_each = var.enabled ? var.cloudwatch_alarms : {}
-
-  alarm_name          = each.value.alarm_name != null ? each.value.alarm_name : each.key
-  comparison_operator = each.value.comparison_operator
-  evaluation_periods  = each.value.evaluation_periods
-  metric_name         = each.value.metric_name
-  namespace           = each.value.namespace
-  period              = each.value.period
-  statistic           = each.value.statistic
-  threshold           = each.value.threshold
-  alarm_description   = each.value.alarm_description
-  alarm_actions       = [each.value.sns_topic_arn]
 }
