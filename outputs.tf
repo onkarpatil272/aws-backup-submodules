e13a0ca65eb43f07dbd995b6c3f0b164fe11d1aa
omi <omi@gmail.com>
2025-07-29 16:14:24 +0530

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,36 +1,36 @@
-# Vault outputs
+# Backup Vault Outputs
 
 output "vault_id" {
   description = "The name of the backup vault"
-  value       = length(aws_backup_vault.backup_vault) > 0 ? aws_backup_vault.backup_vault[0].id : null
+  value       = try(values(aws_backup_vault.backup_vault)[0].id, null)
 }
 
 output "vault_arn" {
   description = "The ARN of the backup vault"
-  value       = length(aws_backup_vault.backup_vault) > 0 ? aws_backup_vault.backup_vault[0].arn : null
+  value       = try(values(aws_backup_vault.backup_vault)[0].arn, null)
 }
 
 output "vault_recovery_points" {
   description = "The number of recovery points stored in the backup vault"
-  value       = length(aws_backup_vault.backup_vault) > 0 ? aws_backup_vault.backup_vault[0].recovery_points : null
+  value       = try(values(aws_backup_vault.backup_vault)[0].recovery_points, null)
 }
 
-# Fixed: Reference to correct resource names
+# Backup Plan Outputs
+
 output "plan_id" {
   description = "The id of the backup plan"
-  value       = length(aws_backup_plan.backup_plan) > 0 ? values(aws_backup_plan.backup_plan)[0].id : null
+  value       = try(values(aws_backup_plan.backup_plan)[0].id, null)
 }
 
 output "plan_arn" {
   description = "The ARN of the backup plan"
-  value       = length(aws_backup_plan.backup_plan) > 0 ? values(aws_backup_plan.backup_plan)[0].arn : null
+  value       = try(values(aws_backup_plan.backup_plan)[0].arn, null)
 }
 
 output "plan_version" {
-  description = "Unique, randomly generated, Unicode, UTF-8 encoded string that serves as the version ID of the backup plan"
-  value       = length(aws_backup_plan.backup_plan) > 0 ? values(aws_backup_plan.backup_plan)[0].version : null
+  description = "Version ID of the backup plan"
+  value       = try(values(aws_backup_plan.backup_plan)[0].version, null)
 }
-
 
 output "plans" {
   description = "Map of backup plans created"
@@ -43,17 +43,15 @@ output "plans" {
   }
 }
 
-
 output "plan_role" {
   description = "The service role used by the backup plan"
   value       = var.iam_role_arn
 }
 
 output "vault_kms_key_arn" {
-  description = "The server-side encryption key that is used to protect your backups"
-  value       = try(aws_backup_vault.backup_vault[0].kms_key_arn, null)
+  description = "The server-side encryption key used for vault"
+  value       = try(values(aws_backup_vault.backup_vault)[0].kms_key_arn, null)
 }
-
 
 output "vault_lock_configuration" {
   description = "The backup vault lock configuration"
@@ -64,28 +62,24 @@ output "vault_lock_configuration" {
   } : null
 }
 
-
 output "backup_selection_ids" {
   description = "Map of backup selection IDs"
   value = {
     for k, v in aws_backup_selection.ab_selection : k => v.id
   }
 }
+
 output "backup_vault_arn" {
   description = "Alias output for backup vault ARN"
-  value       = try(aws_backup_vault.backup_vault[0].arn, null)
+  value       = try(values(aws_backup_vault.backup_vault)[0].arn, null)
 }
 
 output "backup_plan_ids" {
   description = "List of backup plan IDs"
-  value       = [for plan in aws_backup_plan.backup_plan : plan.id]
+  value       = [for plan in values(aws_backup_plan.backup_plan) : plan.id]
 }
 
 output "sns_topic_arns" {
   description = "Map of SNS topic ARNs used for backup notifications"
   value       = local.sns_topic_arns
 }
-
-
-
-
